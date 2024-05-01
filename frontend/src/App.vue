f6c9aff1c1ed2aa2d97e6f53cb236ca15d0bad0a
ARPAN MONDAL <accesstoarpan@gmail.com>
2024-05-02 05:26:56 +0530

    Updated Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <AlgoDeMarket />
+  </div>
+</template>
+
+<script>
+import AlgoDeMarket from './components/AlgoDeMarket.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AlgoDeMarket
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
